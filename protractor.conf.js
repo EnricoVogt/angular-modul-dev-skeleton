3a6bcb703de17df217316b41dc142b9759c47470
EV <EnricoVogt@users.noreply.github.com>
2017-11-06 22:04:00 +0100

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,5 @@
 let util = require("./util.js");
-let testfiles = util.getTestFiles('test-unit.js');
+let testfiles = util.getTestFiles('test-e2e.js');
 
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
